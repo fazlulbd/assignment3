613594108b14c71cfe9f8281fa2a32b41fcf1f56
fazlulbd <fazlulhoque577@gmail.com>
2024-09-28 23:07:40 +0600

    first commit

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name = "Donald Trump";
+        $age = "75";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age
+        ];
+
+        $cookie_name = 'access_token';
+        $cookie_value = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        $cookie = cookie($cookie_name, $cookie_value, $minutes, $path, $domain, $secure, $httpOnly);
+
+        return response()->json($data, 200)->cookie($cookie);
+    }
+}
